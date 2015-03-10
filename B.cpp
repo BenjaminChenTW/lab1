5e3530d5bb6c12924ca4851721a9870117a1c194
student <H34031039@2015cpp.(none)>
2015-03-10 23:04:39 +0800

    Final

--- B.cpp
+++ B.cpp
@@ -5,7 +5,13 @@ int main ()
 {
 	int n;
 	cin >> n;
-	cout << n;
-	while(1)
-	{
-		
+	while(1){
+		cout << n << " ";
+		if (n==1)
+			break;
+		else if (n%2)
+			n=3*n+1;
+		else
+			n=n/2;}
+	cout << endl;
+	return 0;}
